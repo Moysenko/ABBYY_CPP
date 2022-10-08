fe3abf1cace7a6342ba42d608fe99ad5d61d2ce2
Mikhail Moysenko <moysenkom@yandex.ru>
2022-10-08 21:44:21 +0300

    add trash

--- Homework_2/main.cpp
+++ Homework_2/main.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 #include <vector>
 
-#include "generate_parenthess/sol.cpp"
+#include "sudoku_solver/sol.cpp"
 
 int main() {
-  int n;
-  std::cin >> n;
+  int N = 9;
+  std::vector<std::vector<char>> board(N);
+  for (int y = 0; y < N; y++) {
+    std::vector<char> row(N);
+    for (int x = 0; x < N; x++) {
+      std::cin >> row[x];
+    }
+    board[y] = std::move(row);
+  }
 
-  Solution sol;
-  auto ans = sol.generateParenthesis(n);
-  for (auto& ans_i: ans) {
-    std::cout << ans_i << ' ';
+  Solution solution;
+  solution.solveSudoku(board);
+
+  for (int x = 0; x < N; x++) {
+    for (int y = 0; y < N; y++) {
+      std::cout << board[x][y] << ' ';
+    }
+    std::cout << '\n';
   }
 }
